ceb71192b04c3216ca7bc4d5cfcedcd2f6d7ebdd
SofiaaMas <88851045+SofiaaMas@users.noreply.github.com>
2021-08-30 18:01:04 -0500

    Update exercise.py

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,6 +1,23 @@
 
 def main():
     #escribe tu código abajo de esta línea
+    def es_bisiesto(x):
+    bi=x
+    if (bi%4==0):
+        if(bi%100==0):
+            if(bi%400==0):
+                bi=True
+            else:
+                bi=False
+        else:
+            bi=True
+    else:
+        bi:False
+    return bi
+
+año= int(input())
+calculo= es_bisiesto(año)
+print(calculo)
     pass
 
 if __name__=='__main__':
