6cc83013eb3918f676cf169e08a4a3f04133ea6d
SofiaaMas <88851045+SofiaaMas@users.noreply.github.com>
2021-08-30 12:19:07 -0500

    Update exercise.py

--- assignments/07VolumenPrismaRectangular/src/exercise.py
+++ assignments/07VolumenPrismaRectangular/src/exercise.py
@@ -1,6 +1,16 @@
 
 def main():
     #escribe tu código abajo de esta línea
+    base = int(input("Inserta un valor para la base : "))
+    altura = int(input("Inserta un valor para la altura : "))
+    profundidad = float(input("Inserta un valor para la profundidad" ))
+           
+    def area():
+        area_1 = base * altura
+        volumen = area_1 * profundidad
+        return volumen
+    print("El volumen del prisma es : "+ str(area()))
+    pass
     pass
 
 if __name__=='__main__':
